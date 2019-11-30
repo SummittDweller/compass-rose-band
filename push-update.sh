1bd067ee3e16169bf14d6575b0c43bdf691403d3
Digital-Grinnell <digital@grinnell.edu>
2019-11-29 23:45:52 -0600

    Push is fixed

--- push-update.sh
+++ push-update.sh
@@ -3,9 +3,9 @@ cd ~/Documents/GitHub/compass-rose-band
 current=`git symbolic-ref --short -q HEAD`
 git checkout ${current}
 # compile the site before copying to the new image
-hugo
+hugo --ignoreCache --ignoreVendor --minify --debug --verbose
 # perl -i.bak -lpe  'BEGIN { sub inc { my ($num) = @_; ++$num } } s/(build = )(\d+)/$1 . (inc($2))/eg' config.toml
-docker image build --no-cache -t compassrose .
+docker image build -f push-update-Dockerfile --no-cache -t compassrose .
 docker login
 docker tag compassrose summittdweller/compassrose:latest
 docker push summittdweller/compassrose:latest

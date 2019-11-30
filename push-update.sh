6fdac42dc634d46fe9e0409897b5e5fc36d36893
Digital-Grinnell <digital@grinnell.edu>
2019-11-29 20:33:43 -0600

    Push is still broken

--- push-update.sh
+++ push-update.sh
@@ -2,6 +2,8 @@
 cd ~/Documents/GitHub/compass-rose-band
 current=`git symbolic-ref --short -q HEAD`
 git checkout ${current}
+# compile the site before copying to the new image
+hugo
 # perl -i.bak -lpe  'BEGIN { sub inc { my ($num) = @_; ++$num } } s/(build = )(\d+)/$1 . (inc($2))/eg' config.toml
 docker image build --no-cache -t compassrose .
 docker login

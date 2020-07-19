bf05dd693fe37719ce559d51dab7784f784669eb
Mark McFate <markmcfate@Mackenzies-Mac-Mini.local>
2020-07-18 21:59:51 -0500

    changed push-update for scheduling

--- push-update.sh
+++ push-update.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
-# cd ~/Projects/GitHub/compass-rose-band
-current=`git symbolic-ref --short -q HEAD`
-git checkout ${current}
+# cd /Users/markmcfate/Projects/compass-rose-band
+# current=`git symbolic-ref --short -q HEAD`
+# git checkout ${current}
+
+## Login to docker, one time only!
+cat ~/my_password.txt | /usr/local/bin/docker login -u summittdweller --password-stdin
 
 # Compile the site before copying to the new image.  Round 1 = compassroseband.net
-hugo --ignoreCache --ignoreVendor --minify --debug --verbose --baseURL=https://compassroseband.net
+/usr/local/bin/hugo --ignoreCache --ignoreVendor --minify --debug --verbose --baseURL=https://compassroseband.net
 echo "Hugo's compassroseband.net compilation is complete."
 echo "Starting docker image build..."
-docker image build -f push-update-Dockerfile --no-cache -t compassrose .
+/usr/local/bin/docker image build -f push-update-Dockerfile --no-cache -t compassrose .
 echo "docker image build is complete."
-docker login
-docker tag compassrose summittdweller/compassrose1:latest
-docker push summittdweller/compassrose1:latest
+# cat ~/my_password.txt | /usr/local/bin/docker login -u summittdweller --password-stdin
+/usr/local/bin/docker tag compassrose summittdweller/compassrose1:latest
+/usr/local/bin/docker push summittdweller/compassrose1:latest
 
 # Compile the site before copying to the new image.  Round 2 = thecompassroseband.net
-hugo --ignoreCache --ignoreVendor --minify --debug --verbose --baseURL=https://thecompassroseband.net
+/usr/local/bin/hugo --ignoreCache --ignoreVendor --minify --debug --verbose --baseURL=https://thecompassroseband.net
 echo "Hugo's thecompassroseband.net compilation is complete."
 echo "Starting docker image build..."
-docker image build -f push-update-Dockerfile --no-cache -t compassrose .
+/usr/local/bin/docker image build -f push-update-Dockerfile --no-cache -t compassrose .
 echo "docker image build is complete."
-docker login
-docker tag compassrose summittdweller/compassrose2:latest
-docker push summittdweller/compassrose2:latest
+# cat /var/root/my_password.txt | sudo /usr/local/bin/docker login -u summittdweller --password-stdin
+/usr/local/bin/docker tag compassrose summittdweller/compassrose2:latest
+/usr/local/bin/docker push summittdweller/compassrose2:latest
 
 # Compile the site before copying to the new image.  Round 3 = thecompoassroseband.com
-hugo --ignoreCache --ignoreVendor --minify --debug --verbose --baseURL=https://thecompassroseband.com
+/usr/local/bin/hugo --ignoreCache --ignoreVendor --minify --debug --verbose --baseURL=https://thecompassroseband.com
 echo "Hugo's thecompassroseband.com compilation is complete."
 echo "Starting docker image build..."
-docker image build -f push-update-Dockerfile --no-cache -t compassrose .
+/usr/local/bin/docker image build -f push-update-Dockerfile --no-cache -t compassrose .
 echo "docker image build is complete."
-docker login
-docker tag compassrose summittdweller/compassrose3:latest
-docker push summittdweller/compassrose3:latest
+# cat /var/root/my_password.txt | sudo /usr/local/bin/docker login -u summittdweller --password-stdin
+/usr/local/bin/docker tag compassrose summittdweller/compassrose3:latest
+/usr/local/bin/docker push summittdweller/compassrose3:latest
